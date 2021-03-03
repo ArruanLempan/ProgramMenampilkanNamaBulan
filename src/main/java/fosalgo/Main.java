6336d5caffdc6229ded87d45b100fd376b92f4ff
ArruanLempan <72957542+ArruanLempan@users.noreply.github.com>
2021-03-03 22:03:32 +0800

    Update Main.java

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -1,21 +1,17 @@
-package fosalgo;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Sugiarto Cokrowibowo
- */
-public class Main {
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("input----");
         int n = sc.nextInt();
-        
-        //TULISKAN KODEMU DIBAWAH INI
-        
-        
-        
-    }
 
-}
+        //TULISKAN KODEMU DIBAWAH INI
+        String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+        if (n >= 1 && n <= 12) {
+            n--;
+            System.out.println("Output----");
+            System.out.println(namaBulan[n]);
+            System.out.println(namaBulan[(++n) % 12]);
+            System.out.println(namaBulan[(++n) % 12]);
+            System.out.println(namaBulan[(++n) % 12]);
+
+        }
+    }
